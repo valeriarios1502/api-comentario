d8ff2799d26ac031b4987bbee062bade15f5089a
valeriarios1502 <valeria.rios@utec.edu.pe>
2026-06-17 19:41:32 -0500

    Update comentario.py

--- comentario.py
+++ comentario.py
@@ -8,6 +8,7 @@ def lambda_handler(event, context):
     tenant_id = event['body']['tenant_id']
     texto = event['body']['texto']
     nombre_tabla = os.environ["TABLE_NAME"]
+    nombre_bucket = os.environ["BUCKET_NAME"]
     # Proceso
     uuidv1 = str(uuid.uuid1())
     comentario = {
@@ -20,6 +21,10 @@ def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(nombre_tabla)
     response = table.put_item(Item=comentario)
+
+    s3 = boto3.client('s3')
+    nombre_archivo_s3 = f'{nombre_bucket}/{tenant_id}'
+    s3.put_object(Bucket=nombre_bucket, Key=nombre_archivo_s3, Body=json.dumps(comentario), ContentType='application/json')
     # Salida (json)
     print(comentario)
     return {
